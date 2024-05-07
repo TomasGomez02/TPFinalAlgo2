430ae2de5be3873768f1a8aae530a45b876e4c19
TomasGomez02 <tagomez@estudiantes.unsam.edu.ar>
2024-05-07 16:23:22 -0300

    Añadido BaseTree

--- src/_typing.py
+++ src/_typing.py
@@ -3,5 +3,6 @@ import pandas as pd
 import numpy.typing
 from scipy.sparse import spmatrix
 
+DecisionResult = str | float
 ArrayLike = numpy.typing.ArrayLike
 MatrixLike = np.ndarray | pd.DataFrame | spmatrix
---
+++ src/base_tree.py
@@ -0,0 +1,40 @@
+from _typing import *
+from typing import Optional
+from abc import ABC, abstractmethod
+
+class DecisionBase(ABC):
+    def __init__(self, attribute, values: ArrayLike):
+        self.atribute = attribute
+        self.values: ArrayLike = values
+    
+    @abstractmethod
+    def make_choice(self, X: ArrayLike) -> str:
+        raise NotImplementedError()
+    
+class NumericDecision(DecisionBase):
+    def make_choice(self, X: ArrayLike) -> str:
+        ...
+        
+class CategoricDecision(DecisionBase):
+    def make_choice(self, X: ArrayLike) -> str:
+        ...
+
+class BaseTree:
+    def __init__(self, value):
+        self.value: DecisionBase | DecisionResult = value
+        self.forest: dict[str, BaseTree] = dict()
+        
+    def is_leaf(self):
+        return self.forest == {}
+    
+    def height(self):
+        if self.is_leaf():
+            return 1
+        else:
+            return 1 + max([self.forest[key].height() for key in self.forest.keys()])
+        
+    def insert_tree(self, value: str, tree: "BaseTree"):
+        self.forest[value] = tree
+    
+    
+        
