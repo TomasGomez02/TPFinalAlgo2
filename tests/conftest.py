9e0597c5b1ab22c84c98527ab5d22af8ad41ea30
gonza <gajara@estudiantes.unsam.edu.ar>
2024-06-11 18:40:15 -0300

    tests basicos

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,16 @@ def overFittedTree():
     Y = df["play"]
     tree = DecisionTreeClassifier()
     return tree.fit(X, Y)
+
+@pytest.fixture(scope="session")
+def DF():
+    df = pandas.read_csv("play_tennis.csv")
+    return df
+
+@pytest.fixture(scope="session")
+def X(DF):
+    return DF.drop("play", axis=1)
+
+@pytest.fixture(scope="session")
+def Y(DF):
+    return DF["play"]
---
+++ tests/test_DecisionTree.py
@@ -0,0 +1,33 @@
+from treeModels import DecisionTreeClassifier
+import pytest
+import pandas as pd
+from collections import Counter
+
+def test_overfitted(X, Y):
+    overFittedTree = DecisionTreeClassifier()
+    overFittedTree.fit(X.iloc[2:, :], Y[2:])
+    assert overFittedTree.get_depth() == 5
+    assert Counter(overFittedTree.predict(X.loc[0:2,:])) == {"No":2, "Yes":1}
+    # overFittedTree.predict_proba() == ???
+
+def test_params(X, Y):
+    print(DecisionTreeClassifier(min_samples_split=5).fit(X, Y))
+    assert DecisionTreeClassifier(max_depth=3).fit(X, Y).get_depth() == 3
+    assert len(DecisionTreeClassifier(min_samples_leaf=3)
+               .fit(X, Y).tree.forest["Overcast"].samples) >= 3
+    assert len(DecisionTreeClassifier(min_samples_split=5).fit(X, Y).tree.samples) >= 5
+    # falta algo que vea min_samples_leaf mejor
+
+@pytest.mark.xfail
+def test_C4_5():
+    df = pd.read_csv("CarEval.csv")
+    X = df.drop("class values", axis=1)
+    Y = df["class values"]
+    model = DecisionTreeClassifier(algorithm="C4.5").fit(X.iloc[5:,:], Y.iloc[5:])
+    model.predict(X.iloc[0:5,:])
+    
+
+def test_Exceptions_Errors(X, Y):
+    overFittedTree = DecisionTreeClassifier()
+    with pytest.raises(ValueError):
+        overFittedTree.predict(X)
