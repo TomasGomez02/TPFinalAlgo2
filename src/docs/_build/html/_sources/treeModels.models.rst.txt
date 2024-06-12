6411e66668be1f8a3f73cf6c6c60762a33bc2ef8
SantiagoDarnes <santi.darnes@gmail.com>
2024-06-12 20:26:32 -0300

    Docu Sphinx

---
+++ src/docs/_build/html/_sources/treeModels.models.rst.txt
@@ -0,0 +1,29 @@
+treeModels.models package
+=========================
+
+Submodules
+----------
+
+treeModels.models.model module
+------------------------------
+
+.. automodule:: treeModels.models.model
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+treeModels.models.tree\_models module
+-------------------------------------
+
+.. automodule:: treeModels.models.tree_models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: treeModels.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ src/docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Tree Models'
+copyright = '2024, Victoria Arrudi, Santiago Darnes, Tomas Gomez, Gonzalo Jara, Florencia Montti'
+author = 'Victoria Arrudi, Santiago Darnes, Tomas Gomez, Gonzalo Jara, Florencia Montti'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo', 'sphinx_rtd_theme', 'sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
