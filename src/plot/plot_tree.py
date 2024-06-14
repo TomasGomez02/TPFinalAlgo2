400019fe1b52517b3b6f43cade681c424b4ade2c
SantiagoDarnes <santi.darnes@gmail.com>
2024-06-14 09:05:04 -0300

    Update plot_tree.py

--- src/plot/plot_tree.py
+++ src/plot/plot_tree.py
@@ -14,12 +14,12 @@ class PlotTree:
         colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928']
         node_width = 200
         node_height = 70
-        fill_color = random.choice(colors)  # Choose a random color
+        fill_color = random.choice(colors)
         canvas.create_rectangle(x - node_width / 2, y - node_height / 2, x + node_width / 2, y + node_height / 2, fill=fill_color, outline='black')
         canvas.create_text(x, y, text=text, fill='black', justify='center')
 
     def draw_arrow(self, canvas, x1, y1, x2, y2, text):
-        arrow_y_offset = 35  # Offset for the arrow to be drawn below the node
+        arrow_y_offset = 35 
         canvas.create_line(x1, y1 + arrow_y_offset, x2, y2 - arrow_y_offset, arrow=tk.LAST, fill='black')
         canvas.create_text((x1 + x2) / 2, ((y1 + arrow_y_offset) + (y2 - arrow_y_offset)) / 2, text=text, fill='black', font=('Arial', 10, 'bold'))
 
@@ -33,19 +33,19 @@ class PlotTree:
 
         if not self.tree.is_leaf():
             num_children = len(self.tree.forest)
-            child_dx = dx / max(num_children, 1)  # Adjust horizontal spacing based on number of children
+            child_dx = dx / max(num_children, 1)
 
             for i, key in enumerate(self.tree.forest.keys()):
-                new_x = x + (i - (num_children - 1) / 2) * (child_dx + 250)  # Increase distance between nodes
-                new_y = y + 150  # Increase vertical distance
+                new_x = x + (i - (num_children - 1) / 2) * (child_dx + 250)
+                new_y = y + 150
                 self.draw_arrow(canvas, x, y, new_x, new_y, f'{key}')
                 PlotTree(self.tree.forest[key]).plot(canvas, new_x, new_y, child_dx / 2)
 
     def show(self):
         root = tk.Tk()
-        root.geometry("1600x1200")  # Increase the window size for better visualization
+        root.geometry("1600x1200")
 
-        canvas = tk.Canvas(root, bg='white')  # Set background color to white
+        canvas = tk.Canvas(root, bg='white') 
         canvas.pack(side="top", fill="both", expand=True)
 
         scrollbar_x = tk.Scrollbar(root, orient="horizontal", command=canvas.xview)
@@ -57,7 +57,7 @@ class PlotTree:
         canvas.configure(yscrollcommand=scrollbar_y.set, xscrollcommand=scrollbar_x.set)
         canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
-        self.plot(canvas, 800, 50, 1200)  # Start plotting with more initial horizontal space
+        self.plot(canvas, 800, 50, 1200) 
 
         root.mainloop()
 
